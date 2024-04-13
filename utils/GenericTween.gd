850451ead29718fe6b011b0b3362b4d6dce6bd69
Guy Unger <guyunger@gmail.com>
2024-04-13 11:52:07 +0200

    tween changes

--- utils/GenericTween.gd
+++ utils/GenericTween.gd
@@ -1,18 +1,19 @@
 extends Node
 class_name GenericTween
 
-static func shake(node:Node2D) -> Tween:
+static func shake(node:Node2D, cycles := 5) -> Tween:
 	var tween = node.create_tween()
-	for i in 5:
-		tween.tween_property(node, "position:x", node.position.x - 5, 0.1)
-		tween.tween_property(node, "position:x", node.position.x + 5, 0.1)
+	for i in cycles:
+		tween.tween_property(node, "position:x", - 5, 0.06).as_relative().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
+		tween.tween_property(node, "position:x", + 5, 0.06).as_relative().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
 	
 	return tween
 
 
 static func squish(node:Node2D) -> Tween:
-	var tween = node.create_tween()
-	tween.tween_property(node, "scale:y", 0.9, 0.1)
-	tween.tween_property(node, "scale:y", 1.0, 0.1)
+	var tween = node.create_tween().set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(node, "scale", Vector2(0.95, 1.05), 0.1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(node, "scale", Vector2(1.02, 0.98), 0.1).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(node, "scale", Vector2.ONE, 0.1).set_ease(Tween.EASE_IN_OUT)
 	
 	return tween
