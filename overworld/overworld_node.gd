5c2ed3051438e1e6c70e5a833b948fb72ac4fe20
Guy Unger <guyunger@gmail.com>
2024-04-13 16:42:00 +0200

    overworld levels

--- overworld/overworld_node.gd
+++ overworld/overworld_node.gd
@@ -7,9 +7,6 @@ extends Node2D
 
 enum Type{BATTLE, BATTLE_PATH, MINI_1, MINI_2, MINI_3}
 
-@export var type: Type
-@export_file("*.tscn") var scene := ""
-
 var completed := false
 
 func _ready() -> void:
@@ -17,15 +14,6 @@ func _ready() -> void:
 		for neighbor in neighbors:
 			if not neighbor.neighbors.has(self):
 				neighbor.neighbors.push_back(self)
-	match type:
-		Type.MINI_1:
-			$Sprite.modulate = Color.GREEN
-		Type.MINI_2:
-			$Sprite.modulate = Color.BLUE
-		Type.MINI_3:
-			$Sprite.modulate = Color.RED
-		Type.BATTLE_PATH:
-			$Sprite.modulate = Color.GRAY
 
 func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
@@ -37,9 +25,4 @@ func _draw() -> void:
 
 
 func _on_area_brush_2d_mouse_clicked() -> void:
-	match type:
-		Type.BATTLE, Type.BATTLE_PATH:
-			Game.game.open_battle(scene)
-		Type.MINI_1, Type.MINI_2, Type.MINI_3:
-			Game.game.open_minigame(scene)
-			Game.game.transition.set_circle_position(global_position)
+	Game.game.open_battle("res://battle/battles/%s.tscn" % name.to_snake_case())
