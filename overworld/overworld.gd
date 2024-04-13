bf2287d404ac36aa5f1444350fc2625957ca9365
Guy Unger <guyunger@gmail.com>
2024-04-13 17:42:40 +0200

    countries

--- overworld/overworld.gd
+++ overworld/overworld.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
 func _ready() -> void:
-	%CountGold.text = str(Game.game.gold)
-	%CountSouls.text = str(Game.game.souls)
-	%CountWorms.text = str(Game.game.worms)
+	pass
 
 func _on_button_button_down() -> void:
 	Game.game.open_shop()
