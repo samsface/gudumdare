5d7765db808157cd717d5b111837b7de7229439b
Guy Unger <guyunger@gmail.com>
2024-04-14 21:34:47 +0200

    more pickups and movement

--- battle/pickup_worm.gd
+++ battle/pickup_worm.gd
@@ -2,7 +2,10 @@ extends Node2D
 
 var added = false
 var speed := 1.0
-var life := 10.0
+var life := 0.0
+var velocity: Vector2
+func _ready() -> void:
+	scale = Vector2.ONE * 0.8
 func _process(delta: float) -> void:
 	if added:
 		speed += delta * 20.0
@@ -11,13 +14,16 @@ func _process(delta: float) -> void:
 			queue_free()
 			Game.worms += 1
 	else:
-		life -= delta
-		if life < 2.0:
+		life += delta
+		if life > 8.0:
 			visible = fmod(life/ 0.1, 1.0) > 0.5
-		if life <= 0.0:
+		if life > 10.0:
 			queue_free()
-		if get_local_mouse_position().length() < 50.0 or get_parent().get_parent().won:
+		if life > 0.2 and get_local_mouse_position().length() < 100.0 or get_parent().get_parent().won:
 			added = true
+			GenericTween.squish(self)
 			$AudioAdd.play()
 			z_index += 10
 			visible = true
+		position += velocity * delta
+		velocity -= velocity * delta * 5.0
