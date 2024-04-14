a01b282eef4cb31011d7bef7245ec4ddbe283a2f
Guy Unger <guyunger@gmail.com>
2024-04-14 08:53:53 +0200

    stupid worm in reward screen

--- battle/reward.gd
+++ battle/reward.gd
@@ -2,6 +2,8 @@ extends Control
 
 signal done
 
+var picked := false
+
 func _hand_area_child_entered_tree(node: Node) -> void:
 	if not node is CardEx:
 		return
@@ -10,6 +12,19 @@ func _hand_area_child_entered_tree(node: Node) -> void:
 	%LocationName.text = "[jiggle amp=0.4 freq=0.5][center]Great choice![/center][/jiggle]"
 	%ArrowInstruction.visible = false
 	%AudioPicked.play()
+	
+	picked = true
+	$wowworm/Brush2D.hide()
+	$wowworm/Brush2D2.show()
+	GenericTween.squish($wowworm)
 	await get_tree().create_timer(1.0).timeout
 	
 	done.emit()
+
+var t := 0.0
+func _process(delta: float) -> void:
+	t += delta
+	if picked:
+		$wowworm.rotation = sin(t * PI * 8.0) * 0.02
+	else:
+		$wowworm.rotation = sin(t * PI) * 0.05
