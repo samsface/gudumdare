3ca15c8bc6eb526a5c644e6fbbe3381bd3031657
Guy Unger <guyunger@gmail.com>
2024-04-14 15:18:45 +0200

    .

--- minigames/minigame_burn_the_wood/minigame_burn_the_wood.gd
+++ minigames/minigame_burn_the_wood/minigame_burn_the_wood.gd
@@ -1,43 +1,48 @@
 extends Minigame
 
 
+const STICK = preload("res://minigames/minigame_burn_the_wood/stick.tscn")
+
 @export var number_of_sticks := 20
 
 var stick_count := 0
-var selected_stick: Stick = null
+var selected_stick: Stick
 
 @onready var flames = [
-	$Fire/FlameStart,
-	$Fire/FlameSmall,
-	$Fire/FlameMedium,
-	$Fire/FlameLarge
+	$Fire/Flames/FlameStart,
+	$Fire/Flames/FlameSmall,
+	$Fire/Flames/FlameMedium,
+	$Fire/Flames/FlameLarge
 ]
 
 
+
+
 func _ready() -> void:
+	var rotation_options = [15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180]
+	
 	var spawns = $SpawnPoints.get_children()
 	spawns.shuffle()
-	var sticks = $StickSpawner.spawn(number_of_sticks, spawns)
-
-	for stick: Stick in sticks:
-		stick.stick_selected.connect(handle_stick_selected)
-
+	# Spawn the passed-in number of sticks that the player needs to burn
+	for i in number_of_sticks:
+		var stick: Stick = STICK.instantiate()
+		add_child(stick)
+	
+		# Give the stick a random rotation
+		stick.rotate(deg_to_rad(rotation_options.pick_random()))
+		stick.position = spawns[i].position
+	
 	hide_flames()
-	$Fire/FlameStart.show()
+	$Fire/Flames/FlameStart.show()
 
+var t := 0.0
 
-
-func _physics_process(delta: float) -> void:
-	if selected_stick != null:
-		selected_stick.position = get_global_mouse_position()
-
-
-func handle_stick_selected(stick: Stick):
-	if selected_stick == null:
-		selected_stick = stick
-	elif selected_stick == stick:
-		selected_stick = null
-
+func _process(delta: float) -> void:
+	t += delta * (1.0 + stick_count * 0.2) * 2.0
+	%Flames.scale = Vector2(
+		1.0 + sin(floor(t * 6.0) / 6.0 * TAU) * 0.1,
+		1.0 - sin(floor(t * 6.0) / 6.0 * TAU) * 0.1
+	)
 
 func hide_flames():
 	for f in flames:
@@ -45,10 +50,8 @@ func hide_flames():
 
 
 func _on_fire_area_entered(area: Area2D) -> void:
-	if not selected_stick:
-		return
-		
-	selected_stick.queue_free()
+	
+	area.queue_free()
 	selected_stick = null
 
 	stick_count += 1
@@ -65,13 +68,15 @@ func _on_fire_area_entered(area: Area2D) -> void:
 	
 	var stick_percentage: float = stick_count as float / number_of_sticks as float
 	#print(stick_percentage)
-	if stick_count > 4:#stick_percentage >= 0.8:
-		$Fire/FlameLarge.show()
+	if stick_count > 6:#stick_percentage >= 0.8:
+		$Fire/Flames/FlameLarge.show()
 		%BrushAnimation2D.visible = false
 		%Roasted.visible = true
-	elif stick_count > 2:#stick_percentage >= 0.5:
-		$Fire/FlameMedium.show()
+		GenericTween.squish(%Roasted)
+	elif stick_count > 3:#stick_percentage >= 0.5:
+		$Fire/Flames/FlameMedium.show()
 	elif stick_count > 0:#stick_percentage >= 0.25:
-		$Fire/FlameSmall.show()
+		$Fire/Flames/FlameSmall.show()
 	else:
-		$Fire/FlameStart.show()
+		$Fire/Flames/FlameStart.show()
+
