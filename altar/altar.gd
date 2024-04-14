808fd7382b60c38f9ebbc73661e9736137c1e9ce
Guy Unger <guyunger@gmail.com>
2024-04-14 11:30:47 +0200

    sacrifice table cool

--- altar/altar.gd
+++ altar/altar.gd
@@ -8,6 +8,8 @@ const REQUIRED_SACRIFICE_COUNT = 3
 @onready var new_card_label := $TopPlayer/NewCard/Label
 @onready var sacrifice_slot := $SubViewportContainer/SubViewport/HandTest/SacrificeSlot
 
+var filled := 0.0
+
 func _ready() -> void:
 	#Signals
 	sacrifice_slot.card_dropped.connect(_check_sacrifice_button)
@@ -51,3 +53,14 @@ func _on_confirm_button_pressed() -> void:
 	
 	new_card.hide()
 	sacrifice_button.show()
+
+
+func _process(delta: float) -> void:
+	var filled_to = sacrifice_slot.get_card_children().size()
+	filled = move_toward(filled, filled_to, delta / 0.3)
+	$Background.material.set_shader_parameter("filled", filled)
+	var mouse_off = (get_local_mouse_position() - Game.SCREEN_SIZE * 0.5)
+	$BackBackground.position = mouse_off * 0.01 + Vector2(-30, -20)
+	#$Background.position = -mouse_off * 0.02 + Vector2(-30, -20)
+	$Foreground.position = -mouse_off * 0.02 + Vector2(-35, -20)
+	$Camera2D.position = mouse_off * 0.02 + Game.SCREEN_SIZE * 0.5
