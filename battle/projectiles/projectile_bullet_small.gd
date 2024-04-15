3290f32b449242a2ca799471112d4b6eeb919657
Guy Unger <guyunger@gmail.com>
2024-04-15 11:00:08 +0200

    setup for player cards and units

--- battle/projectiles/projectile_bullet_small.gd
+++ battle/projectiles/projectile_bullet_small.gd
@@ -2,5 +2,5 @@ extends Projectile
 
 func init(battle: Battle, target: Unit, is_foe: bool):
 	self.battle = battle
-	velocity = position.direction_to(target.position) * 1000.0
+	velocity = position.direction_to(target.position).rotated(randf_range(-1, 1) * 0.5) * 1000.0
 	%Model.rotation = velocity.angle()
