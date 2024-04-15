41b0b1186fbdee1cd2c55086c0131e2df97ed2cb
Guy Unger <guyunger@gmail.com>
2024-04-16 00:08:46 +0200

    audio control styling

--- autoloads/audio_controls.gd
+++ autoloads/audio_controls.gd
@@ -4,12 +4,14 @@ const BUS_SOUND = 1
 const BUS_MUSIC = 2
 
 @onready var button := $Button
+@onready var highlight := $Button/Arrow2
 @onready var controls := $Controls 
 @onready var sound_slider := $Controls/SoundSlider
 @onready var music_slider := $Controls/MusicSlider
 
 func _ready() -> void:
 	controls.hide()
+	highlight.visible = false
 
 func _on_button_pressed():
 	controls.visible = !controls.visible
@@ -27,3 +29,16 @@ func _set_overworld_position():
 func _set_battle_position():
 	button.global_position = Vector2(7, 718)
 	controls.global_position = Vector2(0, -32)
+
+func _process(delta: float) -> void:
+	if controls.visible and button.get_local_mouse_position().length() > 600.0:
+		controls.hide()
+
+
+func _on_button_mouse_entered() -> void:
+	highlight.visible = true
+	$Button/Hover.play()
+
+
+func _on_button_mouse_exited() -> void:
+	highlight.visible = false
